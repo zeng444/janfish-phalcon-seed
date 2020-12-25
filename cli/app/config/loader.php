bfbebd190191c0c5cbefa8a3294192d0f60a7759
Robert <zeng444@163.com>
2020-12-25 19:32:28 +0800

    更新种子程序cli

--- cli/app/config/loader.php
+++ cli/app/config/loader.php
@@ -2,6 +2,8 @@
 $loader = new \Phalcon\Loader();
 $loader->registerDirs([
     CLIENT_PATH.'app'.DIRECTORY_SEPARATOR.'tasks'.DIRECTORY_SEPARATOR,
-    $config->application->modelsDir,
+]);
+$loader->registerNamespaces([
+    'Application\Core' => CORE_PATH,
 ]);
 $loader->register();
--- core/Models/BaseModel.php
+++ core/Models/BaseModel.php
@@ -51,12 +51,11 @@ class BaseModel extends Model
         $modelName = get_called_class();
         $model = new $modelName();
         if (!property_exists($model, $column)) {
-            throw new DbException("property  not exists in $modelName model");
+            throw new DbException("property not exists in $modelName model");
         }
-        $rows = $model->modelsManager->executeQuery("SELECT * FROM ".$modelName." WHERE $column IN  ({letter:array}) ORDER BY FIELD($column, {letter:array})", [
+        return $model->modelsManager->executeQuery("SELECT * FROM ".$modelName." WHERE $column IN  ({letter:array}) ORDER BY FIELD($column, {letter:array})", [
             'letter' => $valueList,
         ]);
-        return $rows;
     }
 
     /**
@@ -72,11 +71,49 @@ class BaseModel extends Model
                 'format' => 'Y-m-d H:i:s',
             ],
             'beforeUpdate' => [
-                'field' => ['updated_at'],
+                'field' => ['updatedAt'],
                 'format' => 'Y-m-d H:i:s',
             ],
         ]));
         $this->addSoftDeleteBehavior();
+    }
+
+    /**
+     * @param array $data
+     * @param bool $duplicateUpdate
+     * @param array $noUpdate
+     * @return bool
+     */
+    public function batchInsertAsDict(array $data, $duplicateUpdate = false, $noUpdate = [])
+    {
+        $db = $this->getWriteConnection();
+        $schema = $this->getSchema();
+        $source = $this->getSource();
+        $bind = [];
+        $sql = [];
+        $data = is_int(key($data)) ? $data : [$data];
+        foreach ($data as $index => $items) {
+            $holder = [];
+            foreach ($items as $name => $value) {
+                $pn = $name.$index;
+                $holder[] = ':'.$pn;
+                $bind[$pn] = $value;
+            }
+            $sql[] = '('.implode($holder, ',').')';
+        }
+        $fields = array_keys($data[0]);
+        $field = '`'.implode($fields, '`,`').'`';
+        $sql = "INSERT INTO `$schema`.`$source` ($field)VALUES".implode($sql, ',');
+        if ($duplicateUpdate) {
+            $duplicateUpdateSql = [];
+            foreach ($fields as $field) {
+                if (!$noUpdate || !in_array($field, $noUpdate)) {
+                    $duplicateUpdateSql[] = "`$field`=VALUES(`$field`)";
+                }
+            }
+            $sql .= 'ON DUPLICATE KEY UPDATE '.implode($duplicateUpdateSql, ',');
+        }
+        return $db->execute($sql, $bind);
     }
 
     /**
